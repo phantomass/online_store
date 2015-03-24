5b1042c4655c4fe7f83fa85f187a4fdce8439e8d
phantomass <ilnurgilfanov7@gmail.com>
2015-03-24 13:57:53 +0400

    init rep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OnlineStore::Application.config.secret_key_base = 'd19ffcaaee5234a5b5974f009e217c014e36abbbb932e7214ebbb15b59a582c105c2105d3e40a9bf0f155c5d281c9d7e24586e2f2e114c7c54056855ad313fd5'
