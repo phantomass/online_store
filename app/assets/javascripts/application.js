1379f9bfd1645cf7b2fb8302940ad9ac2302bd4c
phantomass <ilnurgilfanov7@gmail.com>
2015-04-27 23:10:18 +0400

    add catalog, dotdotdot.jqury

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require jquery.dotdotdot
 //= require global
 //= require_tree .
+
--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -5,3 +5,9 @@
         });
     });
 }(window.jQuery);
+
+$(function() {
+    $('.caption').dotdotdot({
+        after: 'a.readmore'
+    });
+});
