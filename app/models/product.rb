47592a47f7490376beaaa752acf1cde1569351e0
phantomass <ilnurgilfanov7@gmail.com>
2015-05-31 18:19:21 +0400

    S33

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,11 @@
 class Product < ActiveRecord::Base
   has_many :line_items
   belongs_to :category
-  has_attached_file :photo
+  has_attached_file :photo,
+    :storage => :s3,
+    :bucket => 'gilf-ilnur.tk',
+    :path => ":attachment/:id/:style.:extension",
+    :s3_credentials => "#{Rails.root}/config/s3.yml"
 
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 5.megabytes
